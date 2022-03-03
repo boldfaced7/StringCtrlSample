c3163a5153cd6789ea0585af55d386aad8359506
준모 강 <boldfaced7@hanyang.ac.kr>
2022-03-03 09:10:54 +0900

    3/2

--- StringCtrlSample/MyString.cpp
+++ StringCtrlSample/MyString.cpp
@@ -10,12 +10,24 @@ CMyString::CMyString()
 
 CMyString::~CMyString()
 {
-
+	Release();
 }
 
 int CMyString::SetString(const char* pszParam)
 {
 	// TODO: 여기에 구현 코드 추가.
+	if (pszParam == nullptr || 0) {
+		return 0;
+	}
+	while (pszParam[m_nLength] != '\0') {
+		m_nLength++;
+	}
+	m_pszData = new char[m_nLength];
+	for (int i = 0; i < m_nLength; i++) {
+		m_pszData[i] = pszParam[i];
+	}
+	m_pszData[m_nLength] = '\0';
+
 	return 0;
 }
 
@@ -30,4 +42,8 @@ const char* CMyString::GetString()
 void CMyString::Release()
 {
 	// TODO: 여기에 구현 코드 추가.
+	if (m_pszData == nullptr) {
+		return;
+	}
+	delete[] m_pszData;
 }
